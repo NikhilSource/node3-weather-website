1d3bf052c6e9184c8ef4f9ad0c217bc80b122444
Nikhil Naik <Nikhil.Naik@odlindia.com>
2020-07-26 23:49:31 +0530

    Farenheit to celcius

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude , longitude , callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=6555ea698084b0e3ea67dbc49421fefd&query='+ longitude + ',' + latitude+'&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=6555ea698084b0e3ea67dbc49421fefd&query='+ longitude + ',' + latitude+'&units=m'
     
     request({url , json: true} , (error , {body} = {}) => {
         if(error){
